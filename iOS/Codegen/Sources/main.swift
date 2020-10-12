cad8e51fb43b8e05c7f6b5a235cf2edbc7b13f3e
Hal Lee <hal@lee.me>
2020-10-12 16:35:00 -0400

    Add back schema downloading as part of CLI

--- iOS/Codegen/Sources/main.swift
+++ iOS/Codegen/Sources/main.swift
@@ -4,49 +4,59 @@ import Foundation
 
 struct Codegen: ParsableCommand {
 
+	@Option(help: "GraphQL Endpoint")
+	var endpoint = "https://content.qz.com/graphql"
+
 	@Option(help: "Local path of query files")
 	var includes = "Queries/**/*.gql"
 
 	@Option(help: "Local path of schema file")
 	var schema = "Schemas/ContentSchema.json"
 
+	static private let parentURL = FileFinder
+		.findParentFolder()
+		.deletingLastPathComponent() // CodeGen
+		.deletingLastPathComponent() // iOS
+		.deletingLastPathComponent() // quartz/content repo
+
 	mutating func run() throws {
+		guard let endpoint = URL(string: endpoint) else { throw Error.invalidEndpoint }
+
 		let cliFolderURL = FileFinder
 			.findParentFolder()
 			.deletingLastPathComponent() // CodeGen
 			.appendingPathComponent("ApolloCLI", isDirectory: true)
 
-		let output = FileFinder
-			.findParentFolder()
-			.deletingLastPathComponent() // CodeGen
-			.deletingLastPathComponent() // iOS
+		let output = Codegen.parentURL
+			.appendingPathComponent("iOS")
 			.appendingPathComponent("QuartzContent", isDirectory: true)
 			.appendingPathComponent("QuartzContent", isDirectory: false)
 			.appendingPathExtension("swift")
+
+		let options = ApolloSchemaOptions(
+			schemaFileName: URL(fileURLWithPath: schema).deletingPathExtension().lastPathComponent,
+			endpointURL: endpoint,
+			outputFolderURL: URL(fileURLWithPath: schema, relativeTo: Codegen.parentURL).deletingLastPathComponent()
+		)
+		try ApolloSchemaDownloader.run(with: cliFolderURL, options: options)
 
 		try generateSwiftFiles(output, cli: cliFolderURL)
 	}
 
 	private func generateSwiftFiles(_ target: URL, cli: URL) throws {
-		let parentURL = FileFinder
-			.findParentFolder()
-			.deletingLastPathComponent() // CodeGen
-			.deletingLastPathComponent() // iOS
-			.deletingLastPathComponent() // quartz/content repo
-
 		let options = ApolloCodegenOptions(
 			codegenEngine: .typescript,
 			includes: includes,
 			mergeInFieldsFromFragmentSpreads: false,
 			modifier: .public,
 			omitDeprecatedEnumCases: true,
-			operationIDsURL: URL(fileURLWithPath: schema, relativeTo: parentURL)
+			operationIDsURL: URL(fileURLWithPath: schema, relativeTo: Codegen.parentURL)
 				.deletingPathExtension()
 				.appendingPathExtension("operations.json"),
 			outputFormat: .singleFile(atFileURL: target),
-			urlToSchemaFile: URL(fileURLWithPath: schema, relativeTo: parentURL)
+			urlToSchemaFile: URL(fileURLWithPath: schema, relativeTo: Codegen.parentURL)
 		)
-		try ApolloCodegen.run(from: parentURL, with: cli, options: options)
+		try ApolloCodegen.run(from: Codegen.parentURL, with: cli, options: options)
 	}
 
 	enum Error: Swift.Error {
