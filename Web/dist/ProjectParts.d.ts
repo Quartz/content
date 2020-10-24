f65fd4e89ae7e1c002a033d6f7e06f0090652c46
Hal Lee <hal@lee.me>
2020-10-24 10:45:07 -0400

    Remove generated files

--- Web/dist/ProjectParts.d.ts
+++
@@ -1,5 +0,0 @@
-import type * as Types from './types';
-export declare type ProjectPartsFragment = ({
-    __typename?: 'Project';
-} & Pick<Types.Project, 'id' | 'count' | 'description' | 'shortDescription' | 'link' | 'name' | 'slug'>);
-export declare const ProjectPartsFragmentDoc: import("@apollo/client").DocumentNode;
--- Web/dist/VideoParts.d.ts
+++
@@ -1,5 +0,0 @@
-import type * as Types from './types';
-export declare type VideoPartsFragment = ({
-    __typename?: 'VideoData';
-} & Pick<Types.VideoData, 'id' | 'duration' | 'episode' | 'playlistId' | 'season' | 'type'>);
-export declare const VideoPartsFragmentDoc: import("@apollo/client").DocumentNode;
