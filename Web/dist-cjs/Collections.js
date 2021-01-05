1d0b87a6bfe04174f478c5ea66eaa5932bf3f433
GitHub Action <action@github.com>
2021-01-05 21:44:16 +0000

    Add TypeScript types

---
+++ Web/dist-cjs/Collections.js
@@ -0,0 +1,63 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useCollectionsLazyQuery = exports.useCollectionsQuery = exports.CollectionsDocument = void 0;
+const client_1 = require("@apollo/client");
+const CollectionParts_1 = require("./CollectionParts");
+const Apollo = __importStar(require("@apollo/client"));
+exports.CollectionsDocument = client_1.gql `
+    query Collections($first: Int = 10, $after: String) {
+  collections(first: $first, after: $after) {
+    pageInfo {
+      endCursor
+    }
+    nodes {
+      ...CollectionParts
+    }
+  }
+}
+    ${CollectionParts_1.CollectionPartsFragmentDoc}`;
+/**
+ * __useCollectionsQuery__
+ *
+ * To run a query within a React component, call `useCollectionsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useCollectionsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useCollectionsQuery({
+ *   variables: {
+ *      first: // value for 'first'
+ *      after: // value for 'after'
+ *   },
+ * });
+ */
+function useCollectionsQuery(baseOptions) {
+    return Apollo.useQuery(exports.CollectionsDocument, baseOptions);
+}
+exports.useCollectionsQuery = useCollectionsQuery;
+function useCollectionsLazyQuery(baseOptions) {
+    return Apollo.useLazyQuery(exports.CollectionsDocument, baseOptions);
+}
+exports.useCollectionsLazyQuery = useCollectionsLazyQuery;
+//# sourceMappingURL=Collections.js.map
